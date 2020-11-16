Fix double softmax bug

code/algorithm/main.py
@@ -63,7 +63,7 @@ def main(config: argparse.Namespace):
         pretrain_backbone(
             backbone,
             DataLoader(train_data, batch_size=config.batch_size, shuffle=True, num_workers=0),
-            torch.optim.SGD(backbone.parameters(), lr=1e-4),
+            torch.optim.SGD(backbone.parameters(), lr=1e-3),
             torch.nn.CrossEntropyLoss(),  # torch.nn.CrossEntropyLoss(),
             loggers,
             config,
@@ -160,8 +160,8 @@ def pretrain_backbone(
                 labels = F.one_hot(labels, num_classes=class_count).type(torch.float32)
             labels = labels.to(config.device)
             optimiser.zero_grad()
-            noisy_posteriors = backbone(feats)
-            loss = criterion(noisy_posteriors, labels)
+            noisy_posteriors, noisy_activations = backbone(feats)
+            loss = criterion(noisy_activations, labels)
             loss.backward()
 
             optimiser.step()